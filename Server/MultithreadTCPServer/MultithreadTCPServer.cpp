a6911c67238a4bd6cc1eb063c4f049632a8cb1c3
SKMBOSS <zzangjungsu@gmail.com>
2019-09-19 15:23:49 +0900

    수정

--- Server/MultithreadTCPServer/MultithreadTCPServer.cpp
+++ Server/MultithreadTCPServer/MultithreadTCPServer.cpp
@@ -1,13 +1,11 @@
 #pragma comment(lib, "ws2_32")
-#include <WinSock2.h>
+#include <winsock.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <process.h>
 
-#define SERVERPORT	9000
+#define SERVERPORT 9000
 #define BUFSIZE		512
-
-//소켓 함수 오류 출력 후 종료
 
 void err_quit(const char *msg)
 {
@@ -22,8 +20,7 @@ void err_quit(const char *msg)
 	exit(1);
 }
 
-//소켓 함수 오류 출력
-void err_display(const char *msg)
+void err_display(const char* msg)
 {
 	LPVOID lpMsgBuf;
 	FormatMessage(
@@ -31,22 +28,21 @@ void err_display(const char *msg)
 		NULL, WSAGetLastError(),
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	printf("[%s] %s", msg, (char*)lpMsgBuf);
 	LocalFree(lpMsgBuf);
 }
 
-// 클라이언트와 데이터 통신
 DWORD WINAPI ProcessClient(LPVOID arg)
 {
 	SOCKET client_sock = (SOCKET)arg;
 	int retval;
 	SOCKADDR_IN clientaddr;
 	int addrlen;
-	char buf[BUFSIZ + 1];
+	char buf[BUFSIZE + 1];
 
 	//클라이언트 정보 얻기
 	addrlen = sizeof(clientaddr);
-	getpeername(client_sock, (SOCKADDR *)&clientaddr, &addrlen);
+	getpeername(client_sock, (SOCKADDR*)&clientaddr, &addrlen);
 
 	while (1)
 	{
@@ -59,12 +55,9 @@ DWORD WINAPI ProcessClient(LPVOID arg)
 		}
 		else if (retval == 0)
 			break;
-
-		//받은 데이터 출력
 		buf[retval] = '\0';
 		printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr),
 			ntohs(clientaddr.sin_port), buf);
-
 		//데이터 보내기
 		retval = send(client_sock, buf, retval, 0);
 		if (retval == SOCKET_ERROR)
@@ -73,108 +66,72 @@ DWORD WINAPI ProcessClient(LPVOID arg)
 			break;
 		}
 	}
-	//closesocket()
-	closesocket(client_sock);
-	printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트번호=%d\n",
-		inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 
+	//closesocket(0
+	closesocket(client_sock);
+	printf("[TCP 서버] 클라이언트 종료 : IP 주소 =%s, 포트 번호 =%d\n",
+		inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 	return 0;
+
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	int retval;
 
-	//윈속초기화
+	//윈속 초기화
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 
 	//socket()
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);//scok_stream -> TCP  sock_dgrem ->UDP		//af_inet 윈도우소켓
 	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
 	//bind()
 	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); //inaddr_any 아무한테나 받겠다
 	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("bind()");
 
 	//listen()
 	retval = listen(listen_sock, SOMAXCONN);
 	if (retval == SOCKET_ERROR) err_quit("listen()");
 
-	//데이터 통신에 사용할 변수
+	//데이터 통신에 사용할변수
 	SOCKET client_sock;
 	SOCKADDR_IN clientaddr;
 	int addrlen;
-	HANDLE hThread;
 	char buf[BUFSIZE + 1];
+	HANDLE hThread;
 
 	while (1)
 	{
-		//accept()
+		//accept			//대기하는 상태 클라이너트가 접속해야 밑에 코드 진행
 		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+
 		if (client_sock == INVALID_SOCKET)
 		{
 			err_display("accept()");
 			break;
 		}
-
 		//접속한 클라이언트 정보 출력
-		printf("\n[TCP 서버] 클라이언트 접속: IP 주소 %s, 포트번호=%d\n",
+		printf("\n[tcp 서버] 클라이언트 접속 : IP 주소 =%s, 포트번호=%d\n",
 			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-
-		//스레드생성1
-		/*hThread = CreateThread(NULL, 0, ProcessClient,(LPVOID)client_sock, 0, NULL);
-		if (hThread == NULL) { closesocket(client_sock); }
-		else { CloseHandle(hThread); }*/
 
 		// 스레드 생성
 		hThread = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)ProcessClient, (LPVOID)client_sock, 0, NULL);
 		if (hThread == NULL) { closesocket(client_sock); }
 		else { CloseHandle(hThread); }
-
-		//클라이언트와 데이터 통신
-		while (1)
-		{
-			//데이터받기
-			retval = recv(client_sock, buf, BUFSIZE, 0);
-			if (retval == SOCKET_ERROR)
-			{
-				err_display("recv()");
-				break;
-			}
-			else if (retval == 0)
-				break;
-
-			//받은데이터 출력
-			buf[retval] = '\0';
-			printf("[TCP/%s:%d] %s\n", inet_ntoa(clientaddr.sin_addr),
-				ntohs(clientaddr.sin_port), buf);
-
-			//데이터보내기
-			retval = send(client_sock, buf, retval, 0);
-			if (retval == SOCKET_ERROR)
-			{
-				err_display("send()");
-				break;
-			}
-		}
-
-		//closesocket()
-		closesocket(client_sock);
-		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트번호=%d\n",
-			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 	}
-	//closesocket()
+	//closesocekt
 	closesocket(listen_sock);
 
-	//윈속종료
+	//윈속 종료
 	WSACleanup();
 	return 0;
 }
